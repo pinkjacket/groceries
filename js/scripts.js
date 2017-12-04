5ce9ce804084d1d9f2098246fe0a33569561f136
Danyel Murray and James Donlan <james@>
2017-12-04 15:29:43 -0800

    form now takes input and returns it capitalized and in alphabetical order

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
   $("#groceries").submit(function(event) {
-    var groceries = ["thing1", "thing2", "thing3", "thing4", "thing5"];
-    var groceriesCaps = groceries.map(function(grocery) {
-      return grocery.toUpperCase();
-    });
-    alert(groceriesCaps);
-    groceries.forEach(function(grocery) {
+    event.preventDefault();
 
-      var thingInput = $("input#" + favorite).val();
-      $("." + grocery).text(thingInput);
-      $("#results").append("<li>" + grocery + "</li>")
+    var groceries = ["thing1", "thing2", "thing3", "thing4", "thing5"];
+    var items = [];
+    // groceries.forEach(function(grocery) {
+    //   var thingInput = $("input#" + grocery).val();
+    // }
+    groceries.forEach(function(grocery) {
+      items.push($("input#" + grocery).val());
+    });
+    items.sort();
+    console.log(items);
+    var itemsCaps = items.map(function(item) {
+      return item.toUpperCase();
+    });
+    itemsCaps.forEach(function(itemCap) {
+      $("#results").append("<li>" + itemCap + "</li>");
     });
     $("#results").show();
     $("#groceries").hide();
-    event.preventDefault();
   });
 });
